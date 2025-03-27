bdb5110b5763ae5cda514b3814331d8436dbb160
Daksh <goyaldaksh5522@gmail.com>
2025-03-27 22:55:34 +0530

    working login sign up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 // wrporking on ec2 
 const corsOptions = {
-  origin: process.env.FORNTEND_URL,
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP met>
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
